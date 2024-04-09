e422c1063a6197ba03a08b4cc49937f1f5cc87da
hudson-newey <neweyh@qut.edu.au>
2024-04-09 11:16:46 +1000

    Complete unit conversions

--- src/components/models/unitConverters.ts
+++ src/components/models/unitConverters.ts
@@ -1,13 +1,13 @@
 import { RenderCanvasSize, RenderWindow, TwoDSlice } from "./rendering";
 import { AudioModel, AudioSegment } from "./recordings";
-import * as d3 from "d3-scale";
+import * as d3Scale from "d3-scale";
 
-type Seconds = number;
-type Hertz = number;
-type Pixels = number;
+export type Seconds = number;
+export type Hertz = number;
+export type Pixels = number;
 
 export class UnitConverters {
-  public static getRenderWindow(scale: Scales, slice: TwoDSlice): RenderWindow {
+  public static getRenderWindow(scale: Scales, slice: TwoDSlice<number, number>): RenderWindow {
     return new RenderWindow({
       startOffset: UnitConverters.pixelsToSeconds(scale, slice.x0),
       endOffset: UnitConverters.pixelsToSeconds(scale, slice.x1),
@@ -40,23 +40,36 @@ export class UnitConverters {
 export class Scales {
   public constructor() {}
 
-  public temporal!: d3.ScaleLinear<number, number, never>;
-  public frequency!: d3.ScaleLinear<number, number, never>;
+  public temporal!: d3Scale.ScaleLinear<number, number, never>;
+  public frequency!: d3Scale.ScaleLinear<number, number, never>;
 
   // TODO: we need to work out how the render window interacts with this
   // we will probably need additional scales that are not limited to a render windows range
   public renderWindowScale(audioModel: AudioModel, audioSegment: AudioSegment, canvas: RenderCanvasSize): Scales {
     const scales = new Scales();
 
-    scales.temporal = d3
+    scales.temporal = d3Scale
       .scaleLinear()
       .domain([audioSegment.startOffset, audioSegment.startOffset + audioModel.duration])
       .range([0, canvas.width]);
 
-    scales.frequency = d3
+    scales.frequency = d3Scale
       .scaleLinear()
       .domain([0, UnitConverters.nyquist(audioModel)])
       .range([0, canvas.height]);
+
+    return scales;
+  }
+
+  public fractionalScale(renderWindow: RenderWindow) {
+    const scales = new Scales();
+
+    scales.temporal = d3Scale.scaleLinear().domain([renderWindow.startOffset, renderWindow.endOffset]).range([0, 1]);
+
+    scales.frequency = d3Scale
+      .scaleLinear()
+      .domain([renderWindow.lowFrequency, renderWindow.highFrequency])
+      .range([0, 1]);
 
     return scales;
   }
