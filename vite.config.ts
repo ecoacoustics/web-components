496b97a1ee0af4cc360f0b421bef4581f4ed13f2
hudson-newey <grath@duck.com>
2024-05-10 11:45:33 +1000

    Fix resolution of workers from remote resources

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+      "Access-Control-Allow-Origin": "*",
     },
   },
   build: {
