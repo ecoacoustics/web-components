521a2c7c48bb5082696192d37b9acdb23f10426d
hudson-newey <neweyh@qut.edu.au>
2024-10-15 16:34:38 +1000

    Use helper for axes settings

--- src/templates/settings.ts
+++ src/templates/settings.ts
@@ -1,13 +1,11 @@
-import { html, nothing, TemplateResult } from "lit";
+import { html, nothing } from "lit";
 import { SpectrogramOptions } from "../helpers/audio/models";
-import { AxesComponent, AxesOptions } from "index";
+import { AxesOptions } from "index";
 import { SlMenuItem } from "@shoelace-style/shoelace";
 import { colorScales } from "../helpers/audio/colors";
 import { windowFunctions } from "../helpers/audio/window";
 import { ChangeEvent } from "../helpers/types/advancedTypes";
 import { Signal } from "@lit-labs/preact-signals";
-
-type SettingsChangeHandler = (event: CustomEvent<{ item: SlMenuItem }>) => void;
 
 export function settingsTemplateFactory(
   spectrogramOptions?: Signal<SpectrogramOptions>,
@@ -22,102 +20,6 @@ export function settingsTemplateFactory(
       ${spectrogramOptions ? spectrogramSettingsTemplate(spectrogramOptions) : nothing}
       ${axesOptions ? axesSettingsTemplate(axesOptions) : nothing}
     </sl-menu>
-  `;
-}
-
-function discreteDropdownHandler<T>(options: Signal<T>, key: keyof T): SettingsChangeHandler {
-  return (event: CustomEvent<{ item: SlMenuItem }>) => {
-    const newValue = event.detail.item.value as any;
-    options.value = {
-      ...options.value,
-      [key]: newValue,
-    };
-  };
-}
-
-function rangeInputHandler<T>(options: Signal<T>, key: keyof T) {
-  return (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = Number(event.target.value);
-    if (Number.isNaN(newValue)) {
-      throw new Error("Invalid number");
-    }
-
-    options.value = {
-      ...options.value,
-      [key]: newValue,
-    };
-  };
-}
-
-// TODO: make this more generic
-function axesChangeHandler(options: Signal<AxesOptions>) {
-  return (event: CustomEvent<{ item: SlMenuItem }>) => {
-    const menuItem = event.detail.item;
-    const checkboxElement = menuItem.querySelector<HTMLInputElement>("input[type=checkbox]");
-
-    if (!checkboxElement) {
-      throw new Error("No checkbox element found");
-    }
-
-    const key = checkboxElement.name as keyof AxesComponent;
-    const value = checkboxElement.checked;
-
-    options.value = {
-      ...options.value,
-      [key]: value,
-    };
-  };
-}
-
-function discreteSettingsTemplate(
-  text: string,
-  values: string[] | number[] | ReadonlyArray<number | string>,
-  currentValue: string | number | boolean,
-  changeHandler: (event: CustomEvent<{ item: SlMenuItem }>) => void,
-): TemplateResult {
-  return html`
-    <sl-menu-item>
-      ${text}
-      <sl-menu @sl-select="${changeHandler}" slot="submenu">
-        ${values.map(
-          (value) =>
-            html`<sl-menu-item
-              type="${value == currentValue ? "checkbox" : "normal"}"
-              value="${value}"
-              ?checked=${value == currentValue}
-            >
-              ${value}
-            </sl-menu-item>`,
-        )}
-      </sl-menu>
-    </sl-menu-item>
-  `;
-}
-
-function rangeSettingsTemplate(
-  text: string,
-  min: number,
-  max: number,
-  step: number,
-  currentValue: number,
-  changeHandler: any,
-): TemplateResult {
-  return html`
-    <sl-menu-item>
-      ${text}
-      <sl-menu slot="submenu">
-        <label>
-          <input
-            @change="${changeHandler}"
-            type="range"
-            min="${min}"
-            max="${max}"
-            step="${step}"
-            value="${currentValue}"
-          />
-        </label>
-      </sl-menu>
-    </sl-menu-item>
   `;
 }
 
@@ -184,49 +86,135 @@ function axesSettingsTemplate(axesOptions: Signal<AxesOptions>) {
   return html`
     <sl-menu-item>
       Axes
-      <sl-menu @sl-select="${axesChangeHandler(axesOptions)}" slot="submenu">
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showXTitle" ?checked=${currentOptions.showXTitle} />
-            X-Axis Title
-          </label>
-        </sl-menu-item>
-
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showYTitle" ?checked=${currentOptions.showYTitle} />
-            Y-Axis Title
-          </label>
-        </sl-menu-item>
-
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showXAxis" ?checked=${currentOptions.showXAxis} />
-            X-Axis Labels
-          </label>
-        </sl-menu-item>
-
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showYAxis" ?checked=${currentOptions.showYAxis} />
-            Y-Axis Labels
-          </label>
-        </sl-menu-item>
-
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showXGrid" ?checked=${currentOptions.showXGrid} />
-            X-Axis Grid Lines
-          </label>
-        </sl-menu-item>
-
-        <sl-menu-item>
-          <label>
-            <input type="checkbox" name="showYGrid" ?checked=${currentOptions.showYGrid} />
-            Y-Axis Grid Lines
-          </label>
-        </sl-menu-item>
+      <sl-menu slot="submenu">
+        ${checkboxSettingTemplate(
+          "X-Axis Title",
+          currentOptions.showXTitle,
+          checkboxChangeHandler(axesOptions, "showXTitle"),
+        )}
+        ${checkboxSettingTemplate(
+          "Y-Axis Title",
+          currentOptions.showYTitle,
+          checkboxChangeHandler(axesOptions, "showYTitle"),
+        )}
+        ${checkboxSettingTemplate(
+          "X-Axis Labels",
+          currentOptions.showXAxis,
+          checkboxChangeHandler(axesOptions, "showXAxis"),
+        )}
+        ${checkboxSettingTemplate(
+          "Y-Axis Labels",
+          currentOptions.showYAxis,
+          checkboxChangeHandler(axesOptions, "showYAxis"),
+        )}
+        ${checkboxSettingTemplate(
+          "X-Axis Grid Lines",
+          currentOptions.showXGrid,
+          checkboxChangeHandler(axesOptions, "showXGrid"),
+        )}
+        ${checkboxSettingTemplate(
+          "Y-Axis Grid Lines",
+          currentOptions.showYGrid,
+          checkboxChangeHandler(axesOptions, "showYGrid"),
+        )}
       </sl-menu>
     </sl-menu-item>
   `;
+}
+
+function discreteSettingsTemplate(
+  text: string,
+  values: string[] | number[] | ReadonlyArray<number | string>,
+  currentValue: string | number | boolean,
+  changeHandler: (event: CustomEvent<{ item: SlMenuItem }>) => void,
+) {
+  return html`
+    <sl-menu-item>
+      ${text}
+      <sl-menu @sl-select="${changeHandler}" slot="submenu">
+        ${values.map(
+          (value) =>
+            html`<sl-menu-item
+              type="${value == currentValue ? "checkbox" : "normal"}"
+              value="${value}"
+              ?checked=${value == currentValue}
+            >
+              ${value}
+            </sl-menu-item>`,
+        )}
+      </sl-menu>
+    </sl-menu-item>
+  `;
+}
+
+function rangeSettingsTemplate(
+  text: string,
+  min: number,
+  max: number,
+  step: number,
+  currentValue: number,
+  changeHandler: any,
+) {
+  return html`
+    <sl-menu-item>
+      ${text}
+      <sl-menu slot="submenu">
+        <label>
+          <input
+            @change="${changeHandler}"
+            type="range"
+            min="${min}"
+            max="${max}"
+            step="${step}"
+            value="${currentValue}"
+          />
+        </label>
+      </sl-menu>
+    </sl-menu-item>
+  `;
+}
+
+function checkboxSettingTemplate(text: string, checked: boolean, changeHandler: any) {
+  return html`
+    <sl-menu-item>
+      <label>
+        <input @change="${changeHandler}" type="checkbox" name="showXGrid" ?checked=${checked} />
+        ${text}
+      </label>
+    </sl-menu-item>
+  `;
+}
+
+function discreteDropdownHandler<T>(options: Signal<T>, key: keyof T) {
+  return (event: CustomEvent<{ item: SlMenuItem }>) => {
+    const newValue = event.detail.item.value as any;
+    options.value = {
+      ...options.value,
+      [key]: newValue,
+    };
+  };
+}
+
+function rangeInputHandler<T>(options: Signal<T>, key: keyof T) {
+  return (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(event.target.value);
+    if (Number.isNaN(newValue)) {
+      throw new Error("Invalid number");
+    }
+
+    options.value = {
+      ...options.value,
+      [key]: newValue,
+    };
+  };
+}
+
+function checkboxChangeHandler<T>(options: Signal<T>, key: keyof T) {
+  return (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.checked;
+    options.value = {
+      ...options.value,
+      [key]: newValue,
+    };
+  };
 }
