c3f9fc2277d32b7c790a4fdc0194cae299f1d13a
hudson-newey <neweyh@qut.edu.au>
2024-10-29 14:35:48 +1000

    Context tokens are now strings for dynamic import interop

--- src/helpers/constants/contextTokens.ts
+++ src/helpers/constants/contextTokens.ts
@@ -1,3 +1,11 @@
+// we have split the context tokens into a separate file so that they can be
+// imported in esm and cjs modules without importing any other modules
+//
+// we used to have these context tokens bundled with their context providers
+// however, this would result in the some custom elements being defined when we
+// only imported the context tokens. This was because all immediately executable
+// code would be executed when the context token bundle was imported
+
 import { createContext } from "@lit/context";
 import { SubjectWrapper } from "../../models/subject";
 import { VerificationGridInjector, VerificationGridSettings } from "verification-grid/verification-grid";
@@ -6,8 +14,12 @@ export interface IRootContext {
   log: (message: string) => void;
 }
 
-export const gridTileContext = createContext<SubjectWrapper>(Symbol("grid-tile-context"));
-export const rootContext = createContext<IRootContext>(Symbol("rootContext"));
+// we use strings here instead of symbols so that users can use these contexts
+// across different bundles / dynamic imports
+// if the context tokens were symbols, they would have different references
+// across different bundles and dynamic imports
+export const gridTileContext = createContext<SubjectWrapper>("oe-grid-tile-context");
+export const rootContext = createContext<IRootContext>("oe-root-context");
 
-export const verificationGridContext = createContext<VerificationGridSettings>(Symbol("verification-grid-context"));
-export const injectionContext = createContext<VerificationGridInjector>(Symbol("injection-context"));
+export const verificationGridContext = createContext<VerificationGridSettings>("oe-verification-grid-context");
+export const injectionContext = createContext<VerificationGridInjector>("oe-injection-context");
