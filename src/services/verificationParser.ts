05d42c29fe18235e1cf2018998717d175448049c
hudson-newey <neweyh@qut.edu.au>
2024-10-29 14:48:27 +1000

    Improve tag parsing

--- src/services/verificationParser.ts
+++ src/services/verificationParser.ts
@@ -31,18 +31,33 @@ export abstract class SubjectParser extends ModelParser<SubjectWrapper> {
 
     const url = (partialModel.url as string) ?? "";
 
-    let tag: Tag | string;
-    if (typeof partialModel.tag === "string") {
-      tag = { text: partialModel.tag };
-    } else if (partialModel.tag instanceof Array) {
-      const tagText = partialModel.tag.map((tagModel) => tagModel.text as string).join(", ");
-      tag = { text: tagText };
-    } else if (typeof partialModel.tag === "object") {
-      tag = partialModel.tag as Tag;
-    } else {
-      tag = { text: "" };
-    }
+    const tag: Tag = SubjectParser.tagParser(partialModel.tag);
 
     return new SubjectWrapper(original as Subject, url, tag);
+  }
+
+  private static tagParser(subjectTag: any): Tag {
+    const isTagString = typeof subjectTag === "string";
+    if (isTagString) {
+      return { text: subjectTag };
+    }
+
+    const isTagArray = subjectTag instanceof Array;
+    if (isTagArray) {
+      return SubjectParser.tagArrayParser(subjectTag as any[]);
+    }
+
+    const isTagObject = typeof subjectTag === "object";
+    if (isTagObject) {
+      return subjectTag as any;
+    }
+
+    const defaultTag = { text: "" };
+    return defaultTag;
+  }
+
+  private static tagArrayParser(subjectTags: any[]): Tag {
+    const tagText = subjectTags.map((tagModel) => tagModel.text).join(", ");
+    return { text: tagText };
   }
 }
