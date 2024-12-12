10caadf93a361421c2969000be3535d6930b66f1
hudson-newey <neweyh@qut.edu.au>
2024-12-12 15:10:31 +1000

    Add paging animation

--- src/components/bootstrap-modal/slides/paging/paging.ts
+++ src/components/bootstrap-modal/slides/paging/paging.ts
@@ -15,7 +15,12 @@ export class PagingSlide extends AbstractSlide {
     return html`
       <div class="paging-slide html-slide">
         <svg viewBox="0 0 390 230">
-          ${importSprites(gridTile, progressBar, cursorSprite)} ${verificationGridSprite()}
+          ${importSprites(gridTile, progressBar, cursorSprite)}
+
+          <g class="paging-pages">
+            <g class="paging-page-current">${verificationGridSprite()}</g>
+            <g class="paging-page-next">${verificationGridSprite()}</g>
+          </g>
 
           <use href="#progress-bar" x="0" y="80" style="--progress: 90px" />
           <use class="paging-cursor" href="#cursor" x="23" y="190" />
