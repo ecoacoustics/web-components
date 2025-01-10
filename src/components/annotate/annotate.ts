c5ac120030699e3e605cab8553b733329429a878
hudson-newey <grath@duck.com>
2025-01-10 15:54:25 +1000

    Add reactive properties to components

--- src/components/annotate/annotate.ts
+++ src/components/annotate/annotate.ts
@@ -1,13 +1,57 @@
 import { html, LitElement, unsafeCSS } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 import { AbstractComponent } from "../../mixins/abstractComponent";
+import { queryAllDeeplyAssignedElements, queryDeeplyAssignedElement } from "../../helpers/decorators";
+import { SpectrogramComponent } from "spectrogram/spectrogram";
+import { UnitConverter } from "../../models/unitConverters";
+import { Size } from "../../models/rendering";
+import { AnnotationComponent } from "annotation/annotation";
+import { Annotation } from "../../models/annotation";
+import { booleanConverter, enumConverter } from "../../helpers/attributes";
 import annotateStyles from "./css/style.css?inline";
+
+export enum AnnotationTagStyle {
+  HIDDEN = "hidden",
+  EDGE = "edge",
+  SPECTROGRAM_TOP = "spectrogram-top",
+}
 
 @customElement("oe-annotate")
 export class AnnotateComponent extends AbstractComponent(LitElement) {
   public static styles = unsafeCSS(annotateStyles);
 
-  private handleSlotChange(): void {}
+  @property({ type: Boolean, converter: booleanConverter, reflect: true })
+  public readonly = false;
+
+  @property({ type: String, attribute: "tag-style", converter: enumConverter(AnnotationTagStyle) as any })
+  public tagStyle: AnnotationTagStyle = AnnotationTagStyle.EDGE;
+
+  @queryDeeplyAssignedElement({ selector: "oe-spectrogram" })
+  private spectrogram?: SpectrogramComponent;
+
+  @queryAllDeeplyAssignedElements({ selector: "oe-annotation" })
+  private annotationElements?: AnnotationComponent[];
+
+  private unitConverter?: UnitConverter;
+  private annotationModels: Annotation[] = [];
+
+  private handleSlotChange(): void {
+    if (this.spectrogram && this.spectrogram.unitConverters) {
+      this.unitConverter = this.spectrogram.unitConverters.value;
+
+      if (this.unitConverter) {
+        this.unitConverter.canvasSize.subscribe((value) => this.handleCanvasResize(value));
+      }
+    }
+
+    if (this.annotationElements) {
+      this.annotationModels = this.annotationElements.flatMap((element: AnnotationComponent) => element.model);
+    }
+  }
+
+  private handleCanvasResize(value: Size): void {
+    console.debug(value, this.annotationModels);
+  }
 
   public render() {
     return html`
--- src/components/annotation/annotation.ts
+++ src/components/annotation/annotation.ts
@@ -1,11 +1,49 @@
 import { html, LitElement, unsafeCSS } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 import { AbstractComponent } from "../../mixins/abstractComponent";
+import { required } from "../../helpers/decorators";
+import { Annotation } from "../../models/annotation";
+import { Tag } from "../../models/tag";
 import annotationStyles from "./css/style.css?inline";
 
 @customElement("oe-annotation")
 export class AnnotationComponent extends AbstractComponent(LitElement) {
   public static styles = unsafeCSS(annotationStyles);
+
+  @required()
+  @property({ attribute: "low-frequency", type: Number })
+  public lowFrequency!: number;
+
+  @required()
+  @property({ attribute: "high-frequency", type: Number })
+  public highFrequency!: number;
+
+  @required()
+  @property({ attribute: "start-time", type: Number })
+  public startTime!: number;
+
+  @required()
+  @property({ attribute: "end-time", type: Number })
+  public endTime!: number;
+
+  @property({ type: Array })
+  public tags: string[] = [];
+
+  public get tagModels(): Tag[] {
+    return [];
+  }
+
+  public get model(): Readonly<Annotation> {
+    return new Annotation(
+      this.startTime,
+      this.endTime,
+      this.lowFrequency,
+      this.highFrequency,
+      this.tagModels,
+      this,
+      [],
+    );
+  }
 
   public render() {
     return html`
