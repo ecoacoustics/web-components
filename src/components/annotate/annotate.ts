0947159bcc891034cce6cbd90f226be261cb0792
hudson-newey <grath@duck.com>
2025-01-16 15:45:17 +1000

    fix zero height spectrogram having bad host size

--- src/components/annotate/annotate.ts
+++ src/components/annotate/annotate.ts
@@ -4,7 +4,7 @@ import { customElement, property } from "lit/decorators.js";
 import { AbstractComponent } from "../../mixins/abstractComponent";
 import { queryAllDeeplyAssignedElements, queryDeeplyAssignedElement } from "../../helpers/decorators";
 import { SpectrogramComponent } from "../spectrogram/spectrogram";
-import { Pixel, UnitConverter } from "../../models/unitConverters";
+import { UnitConverter } from "../../models/unitConverters";
 import { AnnotationComponent } from "../annotation/annotation";
 import { Annotation } from "../../models/annotation";
 import { booleanConverter, enumConverter } from "../../helpers/attributes";
@@ -13,6 +13,7 @@ import { Tag } from "../../models/tag";
 import { computed, watch } from "@lit-labs/preact-signals";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { when } from "lit/directives/when.js";
+import { CssVariable } from "../../helpers/types/advancedTypes";
 import annotateStyles from "./css/style.css?inline";
 
 export enum AnnotationTagStyle {
@@ -99,6 +100,12 @@ export class AnnotateComponent extends AbstractComponent(LitElement) {
     const temporalDomain = this.unitConverter.temporalDomain.value;
     const frequencyDomain = this.unitConverter.frequencyDomain.value;
 
+    console.debug(
+      this.unitConverter.temporalRange.value,
+      this.unitConverter.frequencyRange.value,
+      this.unitConverter.canvasSize.value,
+    );
+
     // TODO: I suspect that we can combine the superset check and this isVisible
     // math so that we only have to do one calculation
     //
@@ -121,19 +128,18 @@ export class AnnotateComponent extends AbstractComponent(LitElement) {
     return isSupersetOfViewBox;
   }
 
-  private annotationTemplate(model: Annotation): HTMLTemplateResult {
+  private annotationTemplate(model: Annotation, index: number): HTMLTemplateResult {
     if (!this.unitConverter) {
       return html`An error occurred`;
     }
 
     const annotationTags = model.tags.map((tag: Tag) => tag.text);
-    const textHeight: Pixel = 14.5;
 
     const left = computed(() => this.unitConverter!.scaleX.value(model.startOffset));
     const width = computed(() => this.unitConverter!.scaleX.value(model.endOffset) - left.value);
 
-    const top = computed(() => this.unitConverter!.scaleY.value(model.highFrequency) - textHeight);
-    const height = computed(() => this.unitConverter!.scaleY.value(model.lowFrequency) - top.value - textHeight);
+    const top = computed(() => this.unitConverter!.scaleY.value(model.highFrequency));
+    const height = computed(() => this.unitConverter!.scaleY.value(model.lowFrequency) - top.value);
 
     const isTagComponentSource =
       model.reference instanceof AnnotationComponent &&
@@ -151,13 +157,21 @@ export class AnnotateComponent extends AbstractComponent(LitElement) {
 
     // console.debug((model.reference as any).tags.map((x: any) => x.text).join(), model, { left, top, width, height });
 
+    const anchorName: CssVariable = `--bounding-box-anchor-${index}`;
+
     return html`
-      <h2 class="bounding-box-heading" part="annotation-heading">${headingTemplate}</h2>
+      <h2 class="bounding-box-heading" part="annotation-heading" style="position-anchor: ${anchorName};">
+        ${headingTemplate}
+      </h2>
       <aside class="annotation-container" tabindex="0" style="left: ${watch(left)}px; top: ${watch(top)}px;">
         <div
           class="bounding-box"
           part="annotation-bounding-box"
-          style="width: ${watch(width)}px; height: ${watch(height)}px;"
+          style="
+            width: ${watch(width)}px;
+            height: ${watch(height)}px;
+            anchor-name: ${anchorName};
+          "
         ></div>
       </aside>
     `;
@@ -167,8 +181,8 @@ export class AnnotateComponent extends AbstractComponent(LitElement) {
     return html`
       <div id="wrapper-element" class="vertically-fill">
         <div class="annotation-surface">
-          ${map(this.annotationModels, (model) =>
-            when(!this.cullAnnotation(model), () => this.annotationTemplate(model)),
+          ${map(this.annotationModels, (model: Annotation, i: number) =>
+            when(!this.cullAnnotation(model), () => this.annotationTemplate(model, i)),
           )}
         </div>
         <slot @slotchange="${() => this.handleSlotChange()}"></slot>
