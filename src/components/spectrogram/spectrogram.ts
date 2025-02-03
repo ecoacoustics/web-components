7ec094df5ac340c9a39f8eb9d2fedb120c9b9251
hudson-newey <grath@duck.com>
2025-02-03 15:34:11 +1000

    fix all related dev environment bugs
    This is a big milestone of this PR.
    
    All of the bugs related to this PR have been fixed.
    
    I will now run the tests, see if there are any regressions, and fix those bugs.

--- src/components/spectrogram/spectrogram.ts
+++ src/components/spectrogram/spectrogram.ts
@@ -277,9 +277,6 @@ export class SpectrogramComponent extends SignalWatcher(ChromeHost(LitElement)) 
     } else if (this.invalidateSpectrogramSource(change)) {
       this.renderSpectrogram();
     }
-
-    // TODO: Find out why this was originally here
-    // this.resizeCanvas(this.canvas);
   }
 
   public async renderSpectrogram() {
@@ -391,7 +388,7 @@ export class SpectrogramComponent extends SignalWatcher(ChromeHost(LitElement)) 
       throw new Error("Attempted to initialize unit converter before creating audio model");
     }
 
-    const unitConverters = new UnitConverter(
+    this.unitConverters.value = new UnitConverter(
       this.renderWindow,
       this.renderCanvasSize,
       // typescript is correctly throwing an error because if the audio model
@@ -401,12 +398,11 @@ export class SpectrogramComponent extends SignalWatcher(ChromeHost(LitElement)) 
       // happen when this component is working correctly.
       //
       // TODO: as part of a defensive programming practice, we should remove
-      // this "as any" cast and gracefully handle errors where the audio model
+      // the "as any" cast and gracefully handle errors where the audio model
       // suddenly destructs itself
       this.audio as any,
       signal(this.melScale),
     );
-    this.unitConverters.value = unitConverters;
   }
 
   private originalFftSize(): Size {
@@ -498,11 +494,18 @@ export class SpectrogramComponent extends SignalWatcher(ChromeHost(LitElement)) 
     }
 
     if (this.scaling === SpectrogramCanvasScale.ORIGINAL) {
+      this.canvas.style.position = "relative";
       this.canvas.style.height = `${size.height}px`;
       this.canvas.style.width = `${size.width}px`;
     } else if (this.scaling === SpectrogramCanvasScale.NATURAL) {
+      this.canvas.style.position = "relative";
       this.canvas.style.width = "auto";
     } else {
+      /*
+        we want absolute positioning because the relative position point will be
+        the .surface element provided by the ChromeHost mixin
+      */
+      this.canvas.style.position = "absolute";
       this.canvas.style.width = "100%";
     }
   }
