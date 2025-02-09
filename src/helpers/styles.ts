b7aba338b356cde2afdb2fd96af0b669ca1bc10a
hudson-newey <grath@duck.com>
2025-02-10 09:14:31 +1000

    Add mergeStyles helper

--- src/helpers/styles.ts
+++ src/helpers/styles.ts
@@ -1,6 +1,31 @@
-import { CSSResultGroup } from "lit";
+import { CSSResultGroup, unsafeCSS } from "lit";
 
-// we use a lambda function here so that it captures "this" as a context
-export const applyStyleSheets = (classConstructor: any, styleSheets: CSSResultGroup) => {
-  console.debug(classConstructor, styleSheets);
-};
+// /**
+//  * @description
+//  * Injects one or multiple style sheets into a LitElement component.
+//  * This can be useful when creating component mixins or abstract components that
+//  * extend the LitElement class.
+//  */
+// export const applyStyleSheets = (
+//   classConstructor: Component,
+//   styleSheets: string[],
+//   currentStyles: CSSResultGroup,
+// ): void => {
+//   const styles = mergeStyles(styleSheets, currentStyles);
+// };
+
+/**
+ * @description
+ * Merges multiple style sheets into one CSSResultGroup.
+ */
+export function mergeStyles(stylesheets: string[], currentStyles?: CSSResultGroup): CSSResultGroup {
+  let returnedStyles: CSSResultGroup = stylesheets.map((style) => unsafeCSS(style));
+
+  if (Array.isArray(currentStyles)) {
+    returnedStyles = [...returnedStyles, ...currentStyles];
+  } else if (currentStyles !== undefined) {
+    returnedStyles = [...returnedStyles, currentStyles];
+  }
+
+  return returnedStyles;
+}
