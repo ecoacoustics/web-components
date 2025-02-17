9caca7c9f6f2d0fd9795b13a198cb9bd0a099b2f
hudson-newey <grath@duck.com>
2025-02-17 15:46:08 +1000

    Update doc examples

--- docs-src/_includes/layouts/verification.11ty.js
+++ docs-src/_includes/layouts/verification.11ty.js
@@ -30,15 +30,6 @@ const renderVerification = ({ name, content, collections, page }) => {
       <div>
         ${content}
 
-        <details>
-          <summary>
-            Results
-          </summary>
-
-          <output id="decision-output">
-          </output>
-        </details>
-
         <style>
           main {
             max-width: 100%;
@@ -49,37 +40,7 @@ const renderVerification = ({ name, content, collections, page }) => {
           .grid-title {
             text-align: center;
           }
-
-          #decision-output {
-            position: relative;
-            width: 100%;
-          }
-
-          .decision {
-            display: block;
-            color: var(--bs-light);
-            background-color: var(--bs-dark);
-            font-family: var(--bs-font-monospace);
-            padding: 1.5em;
-            border-radius: 0.5em;
-          }
         </style>
-
-        <script>
-          (() => {
-          const verificationGrid = document.getElementById("verification-grid");
-          const outputElement = document.getElementById("decision-output");
-
-          function outputDecision(x) {
-            const element = document.createElement("pre");
-            element.innerText = JSON.stringify(x.detail, null, 2);
-            element.className = "decision";
-            outputElement.appendChild(element);
-          }
-
-          verificationGrid.addEventListener("decision-made", (x) => outputDecision(x));
-          })();
-        </script>
       </div>
     </section>
   `;
