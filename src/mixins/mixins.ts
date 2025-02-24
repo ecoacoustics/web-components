26e5451efe230350029957ad0bcb3120ff25f213
hudson-newey <neweyh@qut.edu.au>
2025-02-24 16:03:06 +1000

    Preparing to update snapshot tests

--- src/mixins/mixins.ts
+++ src/mixins/mixins.ts
@@ -1,8 +1,3 @@
 import { LitElement } from "lit";
 
-// the object union here is a workaround for a TypeScript bug where the compiler
-// doesn't support generic types for mixins
-// by using a union with an object, we can successfully dereference the type
-// see: https://github.com/microsoft/TypeScript/issues/16390
-// see: https://github.com/microsoft/TypeScript/issues/37142
-export type Component = new (...args: any[]) => LitElement & object;
+export type Component = new (...args: any[]) => LitElement;
