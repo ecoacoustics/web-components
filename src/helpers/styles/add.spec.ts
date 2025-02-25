2787a48330217fd3f97883d7018b6f292ab5f0a8
hudson-newey <neweyh@qut.edu.au>
2025-02-25 16:56:39 +1000

    Start writing style helper tests

---
+++ src/helpers/styles/add.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "../../tests/assertions";
+
+test("should correctly add a single stylesheet", () => {});
+
+// In this test I test passing in an array of stylesheets that has a nested
+// array inside it.
+// This is possible because each CSSResultArray item can be a CSSResultArray
+test("should correctly add an array of stylesheets", () => {});
+
+test("should have no operation if an empty array is passed", () => {});
