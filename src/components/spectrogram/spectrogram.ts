fa2fb3fc0f8424ddc86bbc8fd7483fffcfce38a1
hudson-newey <hudsonnewey@gmail.com>
2025-06-09 15:39:14 +1000

    Revert pause event listener

--- src/components/spectrogram/spectrogram.ts
+++ src/components/spectrogram/spectrogram.ts
@@ -761,7 +761,6 @@ export class SpectrogramComponent extends SignalWatcher(ChromeHost(LitElement)) 
         id="media-element"
         src="${this.renderedSource}"
         @play="${() => this.play()}"
-        @pause="${() => this.pause()}"
         @ended="${() => this.stop()}"
         preload="metadata"
         crossorigin="anonymous"
