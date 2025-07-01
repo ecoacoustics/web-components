dab293560e4d40951c8dd96cf69bca8f6d7bd29f
hudson-newey <hudsonnewey@gmail.com>
2025-07-01 15:24:45 +1000

    Add tests
    I tried to mock the verification grid tiles "settings" context because I
    thought it might be useful for future tests.
    
    However, it proved to be too difficult.
    In the interest of time, I have deciced to wrap the tile tests in a 1x1
    grid.

--- src/components/verification-grid-tile/verification-grid-tile.spec.ts
+++ src/components/verification-grid-tile/verification-grid-tile.spec.ts
@@ -1,41 +1,16 @@
 import { verificationGridTileFixture as test } from "./verification-grid-tile.fixture";
-import { expect } from "../../tests/assertions";
+import { setBrowserValue } from "../../tests/helpers";
 
 test.describe("verification grid tile", () => {
-  test.beforeEach(async ({ fixture }) => {
+  test("should use a cursor pointer if it is not the only grid tile", async ({ fixture }) => {
     await fixture.create();
+    await setBrowserValue(fixture.component(), "isOnlyTile", false);
+    await fixture.assertCursor("pointer");
   });
 
-  [false, true].forEach((useShortcutKeys: boolean) => {
-    const shortcutKeysText = useShortcutKeys ? "with shortcut keys" : "key mouse click";
-    const selectionStrategy = (fixture: any) =>
-      useShortcutKeys ? fixture.keyboardSelectSpectrogramTile() : fixture.mouseSelectSpectrogramTile();
-
-    test.describe(`selection ${shortcutKeysText}`, () => {
-      test.skip(`should select the tile ${shortcutKeysText}`, async ({ fixture }) => {
-        await selectionStrategy(fixture);
-        expect(await fixture.isSelected()).toBe(true);
-      });
-
-      test.skip(`should select the tile when the ctrl key is pressed ${shortcutKeysText}`, () => {});
-
-      test.skip(`should select the tile when the shift key is pressed ${shortcutKeysText}`, () => {});
-
-      test.skip(`should select the tile when the ctrl and shift keys are pressed ${shortcutKeysText}`, () => {});
-
-      test.skip(`should not change the selection state if the tile is already selected ${shortcutKeysText}`, () => {});
-
-      test.skip(`should be able to deselect the tile using the ctrl key ${shortcutKeysText}`, () => {});
-    });
+  test("should use a normal cursor if it is the only grid tile", async ({ fixture }) => {
+    await fixture.create();
+    await setBrowserValue(fixture.component(), "isOnlyTile", true);
+    await fixture.assertCursor("auto");
   });
-
-  test.skip("should show selection highlight when selected", () => {});
-
-  test.skip("should show the decision color correctly", () => {});
-
-  test.skip("should show keyboard shortcuts when the alt key is held down", () => {});
-
-  test.skip("should be able to use the spacebar to play audio", () => {});
-
-  test.skip("should be able to use the spacebar to pause audio", () => {});
 });
