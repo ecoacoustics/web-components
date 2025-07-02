328cd5fb5ad0e2a2dc03ab7239ff1b93048b04ca
Hudson Newey <neweyh@qut.edu.au>
2025-07-02 00:01:41 +0000

    fix: Don't show "pointer" cursor on 1x1 grids (#390)
    - Adds isOnlyTile property to oe-verification-grid-tile component
    - Use the auto cursor on when on a 1x1 grid
    - Removes a lot of skipped tests that are currently tested in the verification-grid.e2e.spec tests
    
    Fixes: #386

--- src/components/verification-grid-tile/verification-grid-tile.fixture.ts
+++ src/components/verification-grid-tile/verification-grid-tile.fixture.ts
@@ -1,7 +1,6 @@
 import { Page } from "@playwright/test";
-import { getBrowserValue, waitForContentReady } from "../../tests/helpers";
-import { VerificationGridTileComponent } from "./verification-grid-tile";
-import { test } from "../../tests/assertions";
+import { getBrowserStyle, waitForContentReady } from "../../tests/helpers";
+import { expect, test } from "../../tests/assertions";
 
 class VerificationGridTileFixture {
   public constructor(public readonly page: Page) {}
@@ -9,50 +8,28 @@ class VerificationGridTileFixture {
   public component = () => this.page.locator("oe-verification-grid-tile").first();
   public tileContainer = () => this.component().locator(".tile-container").first();
 
-  public async create() {
-    // because the grid tile component can't work without a spectrogram
-    // it is reasonable to have a spectrogram and grid tile component in these
-    // integration tests
-    await this.page.setContent(`
-      <oe-verification-grid-tile>
-        <oe-spectrogram></oe-spectrogram>
-      </oe-verification-grid-tile>
-    `);
-    await waitForContentReady(this.page, ["oe-verification-grid-tile", "oe-spectrogram"]);
+  public async create(content?: string) {
+    // The verification grid exists in this test fixture so that we don't have
+    // to mock the "settings" context.
+    //
+    // TODO: Figure out how to mock the settings context and remove the
+    // verification grid component.
+    const defaultContent = `
+      <oe-verification-grid grid-size="1">
+        <oe-verification-grid-tile></oe-verification-grid-tile>
+      </oe-verification-grid>
+    `;
+
+    await this.page.setContent(content ?? defaultContent);
+    await waitForContentReady(this.page, ["oe-verification-grid-tile"]);
   }
 
-  public async isSelected(): Promise<boolean> {
-    return await getBrowserValue<VerificationGridTileComponent, boolean>(this.component(), "selected");
+  public async assertCursor(expectedCursor: string) {
+    // getBrowserStyles uses getComputedStyles and can therefore assert that the
+    // correct cursor is applied, even if it comes from the user agent styles.
+    const realizedCursor = await getBrowserStyle(this.tileContainer(), "cursor");
+    expect(realizedCursor).toEqual(expectedCursor);
   }
-
-  // TODO: Fix this
-  // public async getSelectionShortcut(): Promise<string> {
-  //   return (
-  //     ((await getBrowserValue<VerificationGridTileComponent>(this.component(), "shortcuts")) as string[]).at(-1) ?? ""
-  //   );
-  // }
-
-  // public async getDecisionColor() {
-  //   return await getBrowserValue<VerificationGridTileComponent>(this.component(), "color");
-  // }
-
-  // public async getTileStyles() {
-  //   return await getBrowserStyles<HTMLDivElement>(this.tileContainer());
-  // }
-
-  // // actions
-  // public async mouseSelectSpectrogramTile() {
-  //   await this.tileContainer().click({ force: true });
-  // }
-
-  // public async keyboardSelectSpectrogramTile() {
-  //   const keyboardShortcut = await this.getSelectionShortcut();
-  //   await this.page.keyboard.press(keyboardShortcut);
-  // }
-
-  // public async setDecisionColor(value: string) {
-  //   await setBrowserValue<VerificationGridTileComponent>(this.tileContainer(), "color", value);
-  // }
 }
 
 export const verificationGridTileFixture = test.extend<{ fixture: VerificationGridTileFixture }>({
