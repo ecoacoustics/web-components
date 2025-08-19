93817428c02a220b6f081ea46f4c8e965739cb6c
hudson-newey <neweyh@qut.edu.au>
2025-08-19 15:11:37 +1000

    Stronger eslint + TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,39 @@
-{
-  "root": true,
-  "rules": {
-    "strict": "error"
-  },
-  "overrides": [
+module.exports = {
+  root: true,
+  overrides: [
     {
-      "extends": ["eslint:recommended", "plugin:@typescript-eslint/strict", "plugin:lit/recommended"],
-      "files": ["*.ts"],
-      "rules": {
+      extends: ["eslint:recommended", "plugin:@typescript-eslint/strict-type-checked", "plugin:lit/recommended"],
+      plugins: ["@typescript-eslint", "lit"],
+      files: ["src/components/"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        projectService: true,
+      },
+      rules: {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-inferrable-types": "error",
         // "max-len": ["warn", { "code": 120 }]
         "no-console": [
           "error",
           {
-            // TODO: Remove "debug" from this allowed list
-            "allow": ["warn", "error",  "time", "timeEnd", "debug"]
-          }
-        ]
+            allow: ["warn", "error", "time", "timeEnd", "debug"],
+          },
+        ],
       },
-      "settings": {
-        "polyfills": ["navigator.userAgentData"]
-      }
+      settings: {
+        polyfills: ["navigator.userAgentData"],
+      },
     },
     {
-      "extends": "plugin:playwright/recommended",
-      "files": ["*.spec.ts", "*.fixture.ts", "src/tests/**/*.ts"],
-      "excludedFiles": ["node_modules/", "dist/", "@types/"],
-      "parserOptions": {
-        "project": "./tsconfig.spec.json"
+      extends: "plugin:playwright/recommended",
+      files: ["**/*.spec.ts", "**/*.fixture.ts", "src/tests/**/*.ts"],
+      excludedFiles: ["node_modules/", "dist/", "@types/"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.spec.json",
       },
-      "rules": {
+      rules: {
         "@typescript-eslint/await-thenable": "warn",
 
         // We have added the playwright linting rules to protect against common
@@ -41,15 +44,15 @@
         // TODO: we should consider enabling these rules in the future, but
         // I have temporarily disabled stylistic rules to avoid unnecessary
         // work that don't directly prevent bugs.
-        "@typescript-eslint/no-floating-promises":"error",
+        "@typescript-eslint/no-floating-promises": "error",
         "playwright/valid-describe-callback": "off",
         "playwright/valid-title": "off",
         "playwright/expect-expect": "off",
         "playwright/no-skipped-test": "off",
         "playwright/no-conditional-in-test": "off",
         "playwright/no-conditional-expect": "off",
-        "playwright/no-networkidle": "off"
-      }
-    }
-  ]
-}
+        "playwright/no-networkidle": "off",
+      },
+    },
+  ],
+};
