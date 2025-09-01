eb06c618a2edf3a9720164fbbbf7e51d6281f539
hudson-newey <hudsonnewey@gmail.com>
2025-09-01 15:14:31 +1000

    Use web-first assertion for tag text
    Playwright has a lot of nice features like waiting until an element is
    stable until making an assertion, automatically retrying, and performing
    accessability checks.
    
    Our custom toHaveTrimmedText assertion does not use these features
    because it is not web-first.
    I'm hoping that if I use a web-first assertion,this test should stop
    being flaky.

--- src/tests/verification-grid/verification-grid.e2e.spec.ts
+++ src/tests/verification-grid/verification-grid.e2e.spec.ts
@@ -1862,7 +1862,7 @@ test.describe("resuming datasets", () => {
 
     test("should evaluate the decision buttons 'when' conditions", () => {});
 
-    test("should show new tag decisions correctly", async ({ fixture }) => {
+    test.only("should show new tag decisions correctly", async ({ fixture }) => {
       const expectedTagText: string[] = [
         // Where "Koala" was corrected to "Brush Turkey"
         "koala Brush Turkey",
@@ -1876,13 +1876,7 @@ test.describe("resuming datasets", () => {
         "Brush Turkey",
       ];
 
-      // This timeout exists because our custom toHaveTrimmedText assertion is
-      // not "web first" and therefore won't retry like other Playwright browser
-      // assertions.
-      // TODO: Remove this timeout once we implement a "web first" strategy for
-      // toHaveTrimmedText.
-      await fixture.page.waitForTimeout(1_000);
-      await expect(fixture.gridTileTagText()).toHaveTrimmedText(expectedTagText);
+      await expect(fixture.gridTileTagText()).toHaveText(expectedTagText);
 
       const expectedMeterTooltips = [
         ["verification: Koala (false)", "new tag: Brush Turkey"],
