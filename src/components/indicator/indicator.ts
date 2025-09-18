5cab610fff2345aabde4b3cd7e7c6bf13eb3331b
Hudson Newey <neweyh@qut.edu.au>
2025-09-18 09:45:47 +0000

    fix: Don't render indicator without unit converter (#527)
    This fixes visual jank and layout shift of indicators before spectrograms are rendered.
    
    Fixes: #526

--- src/components/indicator/indicator.ts
+++ src/components/indicator/indicator.ts
@@ -1,6 +1,6 @@
 import { computed, ReadonlySignal, watch } from "@lit-labs/preact-signals";
-import { html, LitElement, unsafeCSS } from "lit";
-import { customElement, query } from "lit/decorators.js";
+import { html, LitElement, nothing, unsafeCSS } from "lit";
+import { customElement, query, state } from "lit/decorators.js";
 import { SpectrogramComponent } from "../spectrogram/spectrogram";
 import { UnitConverter } from "../../models/unitConverters";
 import { queryDeeplyAssignedElement } from "../../helpers/decorators";
@@ -32,6 +32,7 @@ export class IndicatorComponent extends ChromeProvider(LitElement) {
 
   // TODO: investigate why I am de-referencing the signal here. Wouldn't it be
   // easier to work with and more performant with a reactive signal
+  @state()
   private unitConverter?: UnitConverter;
   private computedTimePx: ReadonlySignal<number> = computed(() => 0);
 
@@ -75,6 +76,10 @@ export class IndicatorComponent extends ChromeProvider(LitElement) {
   }
 
   public chromeOverlay(): ChromeTemplate {
+    if (!this.unitConverter) {
+      return nothing;
+    }
+
     return html`
       <svg id="indicator-svg">
         <g id="indicator-group" style="transform: translateX(${watch(this.computedTimePx)}px);">
